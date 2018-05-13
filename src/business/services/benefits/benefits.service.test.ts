cc25c98ada177eb15463cbb00175c758b76e1048
aconfee <adamestela@gmail.com>
2018-05-12 22:26:23 -0500

    dumb test to pass deploy

---
+++ src/business/services/benefits/benefits.service.test.ts
@@ -0,0 +1,15 @@
+import chai from 'chai';
+const assert = chai.assert;
+
+describe('Adder', () => {
+
+    describe('Add numbers', () => {
+        it('Can add.', () => {
+            const sum = 3 + 2;
+
+            assert.equal(sum, 5);
+
+        });
+    });
+    
+});
